20b6a5c04dc42703d47e15b879ae47adfc15ae81
bingo-game <friendstech45@gmail.com>
2024-07-15 18:26:09 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/report.js',
+  entry: './report.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
